185840a94643a69d0768b5a2ea0cd3618873b3b9
LapaKWsb <128227172+LapaKWsb@users.noreply.github.com>
2023-06-16 14:36:26 +0200

    PodetekBelki.cs

---
+++ PodetekBelki.cs
@@ -0,0 +1,35 @@
+// See https://aka.ms/new-console-template for more information
+using System;
+
+namespace Zadanie4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Początkowa kwota: ");
+            decimal Poczatkowa = Convert.ToDecimal(Console.ReadLine());
+
+            Console.WriteLine("Oprocentowanie w skali roku: ");
+            decimal oprocentowanie = Convert.ToDecimal(Console.ReadLine());
+
+            Console.WriteLine("Liczba miesięcy oszczędzania: ");
+            int liczbaMiesiecy = Convert.ToInt32(Console.ReadLine());
+
+            decimal kwotaZarobiona = ObliczKwoteZarobiona(Poczatkowa, oprocentowanie, liczbaMiesiecy);
+            Console.WriteLine("Kwota zarobiona: " + kwotaZarobiona);
+
+            Console.ReadLine();
+        }
+
+        static decimal ObliczKwoteZarobiona(decimal Poczatkowa, decimal oprocentowanie, int liczbaMiesiecy)
+        {
+            decimal oprocentowanieMiesieczne = oprocentowanie / 1200;
+            decimal kwotaZarobiona = Poczatkowa * (decimal)Math.Pow(1 + (double)oprocentowanieMiesieczne, liczbaMiesiecy);
+            kwotaZarobiona -= Poczatkowa;
+            kwotaZarobiona *= 0.81m;
+
+            return kwotaZarobiona;
+        }
+    }
+}
