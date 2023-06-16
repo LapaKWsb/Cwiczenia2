78f0fac39aa5b288c48360b7b961064a09919797
LapaKWsb <128227172+LapaKWsb@users.noreply.github.com>
2023-06-16 14:55:27 +0200

    ParyLiczbN.cs

---
+++ ParyLiczbN.cs
@@ -0,0 +1,22 @@
+// See https://aka.ms/new-console-template for more information
+using System;
+
+namespace Zadanie6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Liczba n: ");
+            int n = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine($"Pary liczb naturalnych o sumie n:");
+            for (int i = 1; i <= n / 2; i++)
+            {
+                Console.WriteLine($"{i} + {n - i} = {n}");
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
